22f7405a38b67cff2f9f998508535161972526d0
hyeon <lhyeonnit@gmail.com>
2024-03-22 12:12:03 +0900

    offer 리팩ㄹ토링

---
+++ src/main/java/shop/mtcoding/blog/model/offer/OfferJPARepository.java
@@ -0,0 +1,7 @@
+package shop.mtcoding.blog.model.offer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferJPARepository extends JpaRepository<Offer, Integer> {
+
+}
