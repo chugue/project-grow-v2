02c28b9ffa9362572504316569ab0b2f27e35174
SCHcoding <codingb007@gmail.com>
2024-03-22 12:02:49 +0900

    resume 컨트롤러 수정 완료

--- src/main/java/shop/mtcoding/blog/model/resume/ResumeController.java
+++ src/main/java/shop/mtcoding/blog/model/resume/ResumeController.java
@@ -2,13 +2,58 @@ package shop.mtcoding.blog.model.resume;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RequiredArgsConstructor
 @Controller
 public class ResumeController {
     private final ResumeService resumeService;
 
+    @GetMapping("/resume/resume-detail/{id}")
+    public String resumeDetail(@PathVariable Integer id) {
 
+        return "/resume/resume-detail";
+    }
+
+    @GetMapping("/resume/{id}/manage-resume")
+    public String manageResume(@PathVariable Integer id) {
+
+        return "/resume/manageResume";
+    }
+
+    @GetMapping("/resume/{id}/write-resume-form")
+    public String writeResumeForm() {
+
+        return "/resume/write-resume-form";
+    }
+
+    @GetMapping("/resume/{id}/update-resume-form")
+    public String updateResumeForm(@PathVariable int id) {
+
+        return "/resume/update-resume-form";
+    }
+
+    @PutMapping("/resume/{id}/update")
+    public String update(@PathVariable Integer id) {
+
+        //해당 부분 redirect 해보고 틀렸으면 본인이 수정
+        return "redirect:/board/" + id;
+    }
+
+    @PostMapping("/resume/save")
+    public String save() {
+
+        // return 부분 manage-resume id 안 받나..? 아무튼 수정해야함. 본인이 작업해보고 수정하길
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/resume/{id}/delete")
+    public String delete(@PathVariable int id) {
+
+        // return 부분 manage-resume id 안 받나..? 아무튼 수정해야함. 본인이 작업해보고 수정하길
+        return "redirect:/";
+
+    }
 
 }
