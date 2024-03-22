fa3f54ecc4ce64e29a5d6b8c61550af89669b79a
yuzusim <yuzusim@gmail.com>
2024-03-22 14:06:59 +0900

    {ãhelp생성

---
+++ src/main/java/shop/mtcoding/blog/model/help/HelpJPARepository.java
@@ -0,0 +1,6 @@
+package shop.mtcoding.blog.model.help;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HelpJPARepository extends JpaRepository<Help, Integer> {
+}
