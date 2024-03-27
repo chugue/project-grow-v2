6ad2a68e61238d454a878de4ef81ec8fcdfd3c16
김지훈 <greenpig4b@gmail.com>
2024-03-27 23:10:26 +0900

    이력사진뿌리기

---
+++ src/main/java/shop/mtcoding/blog/model/file/FileController.java
@@ -0,0 +1,20 @@
+package shop.mtcoding.blog.model.file;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog.model.user.UserService;
+
+@RequiredArgsConstructor
+@Controller
+public class FileController {
+    private final FileService fileService;
+    private final UserService userService;
+
+    @PostMapping("/upload")
+    public String upload(FileInfoRequest.UploadDTO reqDTO){
+        fileService.upload(reqDTO);
+
+        return "redirect:/";
+    }
+}
