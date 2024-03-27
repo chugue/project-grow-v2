8fc4cf55cb41188b203d844ae9d8b958cf4f8796
greenpig4b <greenpig4b@naver.com>
2024-03-27 17:02:26 +0900

    resume delete

--- src/main/java/shop/mtcoding/blog/model/resume/ResumeService.java
+++ src/main/java/shop/mtcoding/blog/model/resume/ResumeService.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import shop.mtcoding.blog._core.errors.exception.Exception401;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
 import shop.mtcoding.blog._core.errors.exception.Exception404;
 import shop.mtcoding.blog.model.apply.Apply;
 import shop.mtcoding.blog.model.apply.ApplyJPARepository;
@@ -24,6 +25,7 @@ public class ResumeService {
     private final ResumeJPARepository resumeJPARepo;
     private final ApplyJPARepository applyJPARepo;
     private final SkillJPARepository skillJPARepo;
+    private final UserJPARepository userJPARepository;
     private final HttpSession session;
 
     public List<ResumeResponse.ResumeApplyDTO> findAllResumeJoinApplyByUserIdAndJobsId(Integer userId, Integer jobsId) {
@@ -44,12 +46,14 @@ public class ResumeService {
         return resumeApplyDTOList;
     }
 
+    //이력서 뿌리기
     public List<ResumeResponse.ResumeDTO> findAll() {
        List<Resume> resumes = resumeJPARepo.findAll();
 
        return resumes.stream().map(resume -> resume.toDTO()).collect(Collectors.toList());
     }
 
+    //이력서 신청
     @Transactional
     public void save(ResumeRequest.SaveDTO saveDTO){
         //1. 인증처리 : 유저가 세션을가지고있는지 로그인상태 확인
@@ -74,9 +78,26 @@ public class ResumeService {
                 .forEach((skill) -> {
                     skillJPARepo.save(skill);
                 });
-
-
     }
 
+    //이력서 삭제
+    @Transactional
+    public void delete(Integer boardId){
+        //1. 인증처리
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        if (sessionUser.getId() == null){
+            throw new Exception401("서비스가 필요한 서비스입니다.");
+        }
+        Resume resume = resumeJPARepo.findById(boardId)
+                .orElseThrow(() -> new Exception404("해당 게시글을 찾을 수 없습니다"));
+        //2.권한처리
+        if (sessionUser.getId() != resume.getUser().getId()){
+            throw new Exception403("해당 게시글을 삭제할 권한이 없습니다");
+        }
+
+        //3. 삭제하기
+        resumeJPARepo.delete(resume);
+    }
 
  }
