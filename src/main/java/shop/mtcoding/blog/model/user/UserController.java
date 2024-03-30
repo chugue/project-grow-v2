733361488d168661d2fa4715702d77d5d854013e
HaSenugJin <egdg7777@gmail.com>
2024-03-30 22:20:46 +0900

    이력서 상세보기 헤더, 공고 상세보기 헤더 수정

--- src/main/java/shop/mtcoding/blog/model/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/model/user/UserController.java
@@ -40,6 +40,7 @@ public class UserController {
             ursList.get(i).setId(i + 1);
         }
         request.setAttribute("ursList", ursList);
+
         return "/user/resume-home";
     }
 
