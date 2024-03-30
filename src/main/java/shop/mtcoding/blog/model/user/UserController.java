0d84b6ab2c61b5b4e00f264183928e7fbc02dc29
SCHcoding <codingb007@gmail.com>
2024-03-31 00:13:15 +0900

    검색 머스태치 완료

--- src/main/java/shop/mtcoding/blog/model/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/model/user/UserController.java
@@ -63,6 +63,8 @@ public class UserController {
             request.setAttribute("jobsKeyword", jobsKeyword);
         }
 
+        request.setAttribute("keyword", keyword);
+
         return "index";
     }
 
